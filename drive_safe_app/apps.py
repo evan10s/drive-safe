9fb769c386f831053d5555afd647b8007056847d
Evan Strat <evan10s@users.noreply.github.com>
2019-01-21 01:51:22 -0500

    Add working app and hello world page

---
+++ drive_safe_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DriveSafeAppConfig(AppConfig):
+    name = 'drive_safe_app'
--- drive_safe_app/urls.py
+++ drive_safe_app/urls.py
@@ -1,4 +1,4 @@
-"""drive_safe URL Configuration
+"""drive_safe_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from drive_safe_app.views import *
 
 urlpatterns = [
-    path('', include('drive_safe_app.urls')),
-    path('', include('django.contrib.auth.urls')),
-    path('admin/', admin.site.urls),
+    path('', hello_world),
 ]
