9874cac1b0f62cd64eef0d23a6df169279b7198d
Evan Strat <evan10s@users.noreply.github.com>
2019-01-26 13:58:28 -0500

    switch to node

--- drive_safe_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DriveSafeAppConfig(AppConfig):
-    name = 'drive_safe_app'
--- drive_safe_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-# Create your views here.
-def hello_world(request):
-    return HttpResponse("Hello World from Mouse Rat")
